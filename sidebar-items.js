4033a99a3ef3f96897818cc4ba8f743eb7ce5e0c
Slushee-a <slushee@duck.com>
2023-05-14 09:01:49 +0200

    Document questionid service

--- sidebar-items.js
+++ sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["clear","create_player","get_players","judge","main","player_count","send_question","update_player"],"mod":["players","questions"],"struct":["clear","create_player","get_players","judge","player_count","send_question","update_player"]};
+window.SIDEBAR_ITEMS = {"fn":["clear","create_player","get_players","judge","main","player_count","questionid","send_question","update_player"],"mod":["players","questions"],"struct":["clear","create_player","get_players","judge","player_count","questionid","send_question","update_player"]};
